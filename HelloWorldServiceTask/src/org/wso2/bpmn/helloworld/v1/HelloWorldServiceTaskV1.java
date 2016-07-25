c0f81695b52871f509ab0d2b71340206f95e27d6
Hasnu zama <hasnuzama.sk@osmosys.asia>
2016-07-25 17:27:46 +0530

    Simple hello world task using WSO2 BPMN

---
+++ HelloWorldServiceTask/src/org/wso2/bpmn/helloworld/v1/HelloWorldServiceTaskV1.java
@@ -0,0 +1,16 @@
+package org.wso2.bpmn.helloworld.v1;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.JavaDelegate;
+/**
+ *  Hello World Service Task- V1. 
+ */
+public class HelloWorldServiceTaskV1 implements JavaDelegate {
+ 
+    @Override
+    public void execute(DelegateExecution arg0) throws Exception {
+        System.out.println("Hello World ...!!!");
+ 
+    }
+ 
+}
